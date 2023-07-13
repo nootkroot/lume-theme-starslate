dea9ad751d9d5518107180bcb60e82e8f8d84b06
nootkroot <arielkhais@hotmail.com>
2023-07-13 00:53:53 -0700

    Initial commit

---
+++ _config.ts
@@ -0,0 +1,46 @@
+import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
+import postcss from "lume/plugins/postcss.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
+import basePath from "lume/plugins/base_path.ts";
+import slugifyUrls from "lume/plugins/slugify_urls.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
+import netlifyCMS from "lume/plugins/netlify_cms.ts";
+import pageFind from "lume/plugins/pagefind.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import feed from "lume/plugins/feed.ts";
+
+const site = lume({
+  location: new URL("https://example.com/"),
+});
+
+site
+  .ignore("README.md")
+  .copy("img")
+  .use(postcss())
+  .use(date())
+  .use(codeHighlight())
+  .use(basePath())
+  .use(sitemap())
+  .use(pageFind({
+    ui: {
+      resetStyles: false,
+    },
+  }))
+  .use(slugifyUrls({ alphanumeric: false }))
+  .use(feed({
+    output: ["/feed.json", "/feed.xml"],
+    query: "type=posts",
+    info: {
+      title: "=site.title",
+      description: "=site.description",
+    },
+    items: {
+      title: "=title",
+      content: "$.post-body",
+    }
+  }))
+  .use(resolveUrls())
+  .use(netlifyCMS({ netlifyIdentity: true }));
+
+export default site;
